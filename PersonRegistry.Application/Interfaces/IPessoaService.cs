d096fa7113a38d9d20a660c8754406f508bbba02
Daniel Souza <danielrsouza10@gmail.com>
2026-08-16 20:19:06 -0300

    interface para servico de operacoes com Pessoa

---
+++ PersonRegistry.Application/Interfaces/IPessoaService.cs
@@ -0,0 +1,17 @@
+﻿using PersonRegistry.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonRegistry.Application.Interfaces
+{
+    public interface IPessoaService
+    {
+        Task<IEnumerable<Pessoa>> ObterTodasAsync();
+        Task<Pessoa?> ObterPorCodigoAsync(int codigo);
+        Task<IEnumerable<Pessoa>> ObterPorUfAsync(string uf);
+        Task<Pessoa> AdicionarAsync(Pessoa pessoa);
+        Task<Pessoa?> AtualizarAsync(int codigo, Pessoa pessoa);
+        Task<bool> ExcluirAsync(int codigo);
+    }
+}
