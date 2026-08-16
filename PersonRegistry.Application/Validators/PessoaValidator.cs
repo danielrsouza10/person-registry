cb50481c1e1bd4c14c98c0eaad565a9d587f3c76
Daniel Souza <danielrsouza10@gmail.com>
2026-08-16 20:20:13 -0300

    validador para cadastro e edicao de pessoa

---
+++ PersonRegistry.Application/Validators/PessoaValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using PersonRegistry.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonRegistry.Application.Validators
+{
+    public class PessoaValidator : AbstractValidator<Pessoa>
+    {
+        public PessoaValidator()
+        {
+            RuleFor(p => p.Nome)
+                .NotEmpty().WithMessage("O nome é obrigatório.")
+                .MaximumLength(100).WithMessage("O nome não pode ter mais de 100 caracteres.");
+
+            RuleFor(p => p.DataNascimento)
+                .NotEmpty().WithMessage("A data de nascimento é obrigatória.")
+                .LessThan(DateTime.Now).WithMessage("A data de nascimento não pode ser no futuro.");
+
+            RuleFor(p => p.Cpf)
+                .NotEmpty().WithMessage("O CPF é obrigatório.")
+                .Length(11, 14).WithMessage("O CPF deve ter entre 11 e 14 caracteres.");
+
+            RuleFor(p => p.Uf)
+                .NotEmpty().WithMessage("A UF é obrigatória.")
+                .Length(2).WithMessage("A UF deve conter exatamente 2 caracteres (Ex: GO, SP).");
+        }
+    }
+}
