3f8c995892489b835a8854a5b07b27e44a521dee
Daniel Souza <danielrsouza10@gmail.com>
2026-08-16 19:35:31 -0300

    Entidade e Interface para Pessoa

---
+++ PersonRegistry.Domain/Interfaces/IPessoaRepository.cs
@@ -0,0 +1,17 @@
+﻿using PersonRegistry.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonRegistry.Domain.Interfaces
+{
+    public interface IPessoaRepository
+    {
+        Task<IEnumerable<Pessoa>> ObterTodasAsync();
+        Task<Pessoa?> ObterPorCodigoAsync(int codigo);
+        Task<IEnumerable<Pessoa>> ObterPorUfAsync(string uf);
+        Task<Pessoa> AdicionarAsync(Pessoa pessoa);
+        Task<Pessoa?> AtualizarAsync(Pessoa pessoa);
+        Task<bool> ExcluirAsync(int codigo);
+    }
+}
