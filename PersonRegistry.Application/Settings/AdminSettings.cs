71666bb0500937f3e4a703d42e0c8d96c0648a3b
Daniel Souza <danielrsouza10@gmail.com>
2026-08-17 11:56:29 -0300

    autenticacao com BCrypt

--- PersonRegistry.Application/Settings/AdminSettings.cs
+++ PersonRegistry.Application/Settings/AdminSettings.cs
@@ -7,6 +7,6 @@ namespace PersonRegistry.Application.Settings
     public class AdminSettings
     {
         public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
     }
 }
